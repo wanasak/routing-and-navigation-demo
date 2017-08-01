0a66d0027fb7660e4912ea5a697ab51c99410544
Wanasak <u510610433@gmail.com>
2017-08-01 22:06:15 +0700

    switchMap operator

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { GithubFollowersService } from '../services/github-followers.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/operator/add';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-github-followers',
@@ -12,23 +14,20 @@ import 'rxjs/add/observable/combineLatest';
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GithubFollowersService, private route: ActivatedRoute) { }
+  constructor(
+    private service: GithubFollowersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    Observable.combineLatest([
-      this.route.paramMap,
-      this.route.queryParamMap
-    ])
-    .subscribe(combined => {
-      const id = +combined[0].get('id');
-      const page = combined[1].get('page');
-      const order = combined[1].get('order');
-      console.log('id:' + id);
-      console.log('page: ' + page + ', order:' + order);
-    });
+    Observable.combineLatest([this.route.paramMap, this.route.queryParamMap])
+      .switchMap(combined => {
+        const id = +combined[0].get('id');
+        const page = combined[1].get('page');
+        const order = combined[1].get('order');
 
-    this.service.getAll()
-      .subscribe(res => this.followers = res);
+        return this.service.getAll();
+      })
+      .subscribe(followers => this.followers = followers);
   }
-
 }
